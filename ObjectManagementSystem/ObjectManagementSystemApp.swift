3dfd0d90dbfa039e0cf5e686807f908956cf79f3
David <momachilles@gmail.com>
2024-04-23 15:22:37 +0200

    Delete commented code.

--- ObjectManagementSystem/ObjectManagementSystemApp.swift
+++ ObjectManagementSystem/ObjectManagementSystemApp.swift
@@ -10,25 +10,10 @@ import SwiftData
 
 @main
 struct ObjectManagementSystemApp: App {
-  /*
-  var sharedModelContainer: ModelContainer = {
-    let schema = Schema([
-      SaalObject.self,
-    ])
-    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-    
-    do {
-      return try ModelContainer(for: schema, configurations: [modelConfiguration])
-    } catch {
-      fatalError("Could not create ModelContainer: \(error)")
-    }
-  }() */
-  
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
-    // .modelContainer(sharedModelContainer)
     .modelContainer(for: SaalObject.self)
   }
 }
